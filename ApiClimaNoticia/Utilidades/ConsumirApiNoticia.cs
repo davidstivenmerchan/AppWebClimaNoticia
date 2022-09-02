27628fa305e2912505bf8ba8e2244b40913a2953
stivenshun <dsmerchan6@misena.edu.co>
2022-09-02 12:58:01 -0500

    Carpeta Utiliades con Archivos que consumen las apis

---
+++ ApiClimaNoticia/Utilidades/ConsumirApiNoticia.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiClimaNoticia.Utilidades
+{
+    public class ConsumirApiNoticia
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public ConsumirApiNoticia(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public string KeyApi = "&from=2022-08-02&sortBy=publishedAt&apiKey=806ac58329494335b05ba960df4c3b80&pageSize=1";
+        public string UrlApi = "https://newsapi.org/v2/everything?q=";
+
+        public async Task<string> getNoticia(string url)
+        {
+            var peticion = new HttpRequestMessage(HttpMethod.Get, url);
+            
+            var cliente = _httpClient.CreateClient();
+
+            HttpResponseMessage respuesta = await cliente.SendAsync(peticion);
+
+            if(respuesta.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonNoticia = respuesta.Content.ReadAsStringAsync();
+                var jsonNoticiaString = jsonNoticia.ToString();
+
+                return jsonNoticiaString;
+            }
+
+            return null;
+        }
+    }
+}
