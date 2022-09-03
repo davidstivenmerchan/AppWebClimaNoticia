4ec525a92b0f6dbb90f3e666ff23b89d2b6d5ef7
stivenshun <dsmerchan6@misena.edu.co>
2022-09-03 10:36:38 -0500

    Componente para mostrar el historial de busqueda y consumo de la api, especificamente el endpoint de mostrar el historial

---
+++ WebClimaNoticia/ClientApp/src/components/TablaHistorial.js
@@ -0,0 +1,18 @@
+﻿import "bootstrap/dist/css/bootstrap.min.css"
+
+
+const TablaHistorial = () => {
+
+    return (
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Ciudad</th>
+                <th>Fecha/Hora</th>
+            </tr>
+        </thead>
+    )
+}
+
+
+export default TablaHistorial;
